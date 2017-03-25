9c40e23e532615176604046001fd0b01d03a82c9
ShenJack <357875929@qq.com>
2017-03-26 00:07:25 +0800

    oops,style sheet error

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+import generic as generic
 from django.http import Http404
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
@@ -6,7 +7,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.template import loader
 from django.urls import reverse
-
+from django.views import generic
 from polls.models import Question, Choice
 
 
@@ -31,6 +32,26 @@ def results(request, question_id):
     return render(request, 'polls/results.html', {
         'question': question
     })
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """"Return the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
